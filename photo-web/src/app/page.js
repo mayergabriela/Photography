208cb24bec3d668475c007aa91622c92fa421672
Ga Mayer <mayergabriela@gmail.com>
2023-12-13 16:55:25 -0300

    Navbar was created. React Icon were installed

--- photo-web/src/app/page.js
+++ photo-web/src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Hero
         heading="ShutterVerse: Capturing Dreams"
         message="Capturing Moments, Creating Memories - Your Story Through My Lens."
